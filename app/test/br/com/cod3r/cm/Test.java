ca1a6184ad1eec57d7b6b1bfd49cae0fa069ad21
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2023-02-24 10:58:52 -0300

    test: add a default eclipse test template
    adding a default template test of eclipse with JUnit that will be used in the future.

---
+++ app/test/br/com/cod3r/cm/Test.java
@@ -0,0 +1,12 @@
+package br.com.cod3r.cm;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test {
+
+	@org.junit.jupiter.api.Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
